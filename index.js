eb291caea04b52f9c120eb6d4bde0e6f7fedc12c
Jimmy Moon <ragingwind@gmail.com>
2016-12-20 00:31:23 +0900

    Add email

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const codeBusking = require('./codebusking');
 module.exports = {
 	firstName: codeBusking.firstName,
 	lastName: codeBusking.lastName,
+	email: codeBusking.email,
 	name: function() {
 		return `${chalk.blue(this.firstName)} ${chalk.red(this.lastName)}`;
 	}
