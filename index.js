19221c290e66bf55a9cf8a3ea63a14600cae91c7
ragingwind <ragingwind@gmail.com>
2016-12-20 15:23:45 +0900

    Add twitter

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const chalk = require('chalk');
 const codeBusking = require('./codebusking');
 
 module.exports = {
-	firstName: codeBusking.firstName, lastName: codeBusking.lastName,
-	email: codeBusking.email, homepage: codeBusking.homepage, github: codeBusking.github,
+	firstName: codeBusking.firstName,
+	lastName: codeBusking.lastName,
+	email: codeBusking.email,
+	homepage: codeBusking.homepage,
+	github: codeBusking.github,
+	twitter: codeBusking.twitter,
 	name: function() {
 		return `${chalk.blue(this.firstName)} ${chalk.red(this.lastName)}`;
 	}
