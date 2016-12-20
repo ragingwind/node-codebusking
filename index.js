41bc43e7fc0c4776223d73611ca65f98ba65ca29
ragingwind <ragingwind@gmail.com>
2016-12-20 14:24:43 +0900

    Add github address

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ module.exports = {
 	lastName: codeBusking.lastName,
 	email: codeBusking.email,
 	homepage: codeBusking.homepage,
+	github: codeBusking.github,
 	name: function() {
 		return `${chalk.blue(this.firstName)} ${chalk.red(this.lastName)}`;
 	}
