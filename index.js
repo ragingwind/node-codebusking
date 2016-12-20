34e6d4c12fbc4b57e21a6040770b7924377689f3
ragingwind <ragingwind@gmail.com>
2016-12-20 15:20:45 +0900

    Align by category

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ const chalk = require('chalk');
 const codeBusking = require('./codebusking');
 
 module.exports = {
-	firstName: codeBusking.firstName,
-	lastName: codeBusking.lastName,
-	email: codeBusking.email,
-	homepage: codeBusking.homepage,
-	github: codeBusking.github,
+	firstName: codeBusking.firstName, lastName: codeBusking.lastName,
+	email: codeBusking.email, homepage: codeBusking.homepage, github: codeBusking.github,
 	name: function() {
 		return `${chalk.blue(this.firstName)} ${chalk.red(this.lastName)}`;
 	}
